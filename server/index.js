59affe0bf0255cb11c52f51dd214ab125e1a03b6
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-12-22 17:46:28 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const app = require('express')();
+
+const port = 6000;
+
+const { test } = require('./db');
+
+function getSentencesWithType(test) {
+    const sentences = test.story.split('. '); // Split the story into an array of sentences
+    const questions = test.questions; // Get the questions object
+    const sentenceObjs = []; // Initialize the array of sentence objects
+    
+    // Iterate through each sentence
+    sentences.forEach(sentence => {
+      let sentenceObj = { sentence: sentence }; // Initialize the sentence object with the sentence
+      
+      for (let key in questions) {
+        if (questions[key].string.includes(sentence)) {
+          sentenceObj.type = "question";
+          sentenceObj.choices = questions[key].choices;
+          sentenceObj.answer = questions[key].answer;
+        }
+      }
+      
+      // If the type property is not set, set it to "basic"
+      if (!sentenceObj.type) {
+        sentenceObj.type = "basic";
+      }
+      
+      // Add the sentence object to the array
+      sentenceObjs.push(sentenceObj);
+    });
+    
+    return sentenceObjs;
+  }
+
+const sentenceObjs = getSentencesWithType(test);
+console.log(sentenceObjs);
+
+app.use(require('cors')());
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+app.listen(port, () => {
+    console.log(`Server is connected at Port ${port}`)
+});
